a17e73f59eba89a770b5cb286a94efea0ab360ad
Vipin Kumar Tripathi <vipin.tripathi@drishtitech.com>
2013-12-18 11:26:41 +0530

    base module updatation module name as afras_base

--- Multicompany_billing/fiscal_journal.py
+++ Multicompany_billing/fiscal_journal.py
@@ -498,16 +498,8 @@ class account_invoice(osv.osv):
             comp_id=self.pool.get('res.company').search(cr,uid,[('name','=',p.name)])
             acc_fiscal_posi=self.pool.get('account.fiscal.position')
             if company_id:
-<<<<<<< HEAD
                acc_fiscal_position=acc_fiscal_posi.search(cr,uid,[('type','=','icb'),('company_id','in',comp_id)])
                if (p.property_account_receivable.company_id and (p.property_account_receivable.company_id.id != company_id)) and (p.property_account_payable.company_id and (p.property_account_payable.company_id.id != company_id)):
-                
-=======
-
-                
-                if (p.property_account_receivable.company_id and (p.property_account_receivable.company_id.id != company_id)) and (p.property_account_payable.company_id and (p.property_account_payable.company_id.id != company_id)):
-
->>>>>>> c849bea350351efe0c5a33550ee2ebe3f51912b2
                     property_obj = self.pool.get('ir.property')
                     rec_pro_id = property_obj.search(cr,uid,[('name','=','property_account_receivable'),('res_id','=','res.partner,'+str(partner_id)+''),('company_id','=',company_id)])
                     pay_pro_id = property_obj.search(cr,uid,[('name','=','property_account_payable'),('res_id','=','res.partner,'+str(partner_id)+''),('company_id','=',company_id)])
@@ -537,15 +529,12 @@ class account_invoice(osv.osv):
             fiscal_position = p.property_account_position and p.property_account_position.id or False
             if p.bank_ids:
                 bank_id = p.bank_ids[0].id
-<<<<<<< HEAD
             if p.is_intragroup_company ==True:
                 value1=True
-=======
 
             if p.is_intragroup_company ==True:
                 value1=True
 
->>>>>>> c849bea350351efe0c5a33550ee2ebe3f51912b2
                
             
             print "user_obj.company_id.is_shared_company",user_obj.company_id.is_shared_company
@@ -554,11 +543,6 @@ class account_invoice(osv.osv):
                   is_intragroup_company = True
             elif user_obj.company_id.is_shared_company:
                 fiscal_type  = 'ss'
-<<<<<<< HEAD
-=======
-
->>>>>>> c849bea350351efe0c5a33550ee2ebe3f51912b2
-                
             elif user_obj.company_id.technology_company:
                 fiscal_type  = 'T'
             fiscal_id =  acc_fiscal_posi.search(cr,uid, [('type','=',fiscal_type),('company_id','=',user_obj.company_id.id)])
